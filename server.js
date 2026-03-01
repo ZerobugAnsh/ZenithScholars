326e81ef4a4dfe2492f10e35296930a59f87c5fb
Ansh Rajeev <rajeevansh00@gmail.com>
2026-03-01 17:09:13 +0530

    login

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(cors({
   origin: "https://zerobugansh.github.io",
   methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.options("*", cors());
